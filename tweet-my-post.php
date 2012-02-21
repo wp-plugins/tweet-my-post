8a4e01c0eb3b15ad9c62b3cf20223fdc63be3df7
ksg91 <ksg91@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-21 10:47:20 +0000

    Tagged version 0.9
    git-svn-id: https://plugins.svn.wordpress.org/tweet-my-post/tags/0.9@508159 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tweet-my-post.php
@@ -0,0 +1,136 @@
+<?php
+/*
+Plugin Name: Tweet My Post
+Plugin URI: https://github.com/ksg91/Tweet-My-Post
+Description: A WordPress Plugin which Tweets the new posts with its Author's Twitter handle. 
+Version: 0.9.4
+Author: Kishan Gor
+Author URI: http://ksg91.com
+License: GPL2
+
+/////////////////////////////////////////////////////////////////////
+    Copyright 2012  Kishan Gor  (email : ego@ksg91.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//////////////////////////////////////////////////////////////////////
+*/
+add_action("admin_menu","add_tmp_page");
+add_action("admin_init","reg_settings");
+add_action('publish_post','tmp_tweet_it');
+function tmp_activate()
+{
+  add_option("twitter-consumer-key","");
+  add_option("twitter-consumer-secret","");
+  add_option("twitter-access-token","");
+  add_option("twitter-access-secret","");
+}
+function tmp_tweet_it($postID)
+{
+  require_once 'lib/EpiCurl.php';
+  require_once 'lib/EpiOAuth.php';
+  require_once 'lib/EpiTwitter.php';
+  $twitterObj = new EpiTwitter(get_option("twitter-consumer-key"), 
+    get_option("twitter-consumer-secret"),get_option("twitter-access-token"),
+    get_option("twitter-access-secret"));
+  $tweet=buildTMPTweet($postID);
+  $update_status = $twitterObj->post_statusesUpdate(array('status' => $tweet ));
+  $update_status->response;
+  return $postID;
+}
+function buildTMPTweet($postID)
+{
+  $post=get_post($postID);
+  $author=get_option("ID-".$post->post_author);
+  $link=get_permalink($postID);
+  $tweet=$author;
+  if($author=="") {
+    $title=$post->post_title;
+    if(strlen($title)>114){
+      substr($title,0,110);
+      $title.="...";
+    }
+    $tweet="\"".$post->post_title."\" - ".$link;
+  }
+  else {
+    $len=strlen(" by @".$author);
+    $title=$post->post_title;
+    if(strlen($title)>(116-$len)){
+      $title=substr($title,0,(110-$len));
+      $title.="...";
+    }
+    $tweet="\"".$title."\" - ". $link." by @".$author;
+  }
+  return $tweet;
+}
+function reg_settings()
+{
+  global $current_user;
+  get_currentuserinfo();
+  register_setting('tmp-option', 'ID-'.$current_user->ID);
+  register_setting('tmp-option', 'twitter-consumer-key');
+  register_setting('tmp-option', 'twitter-consumer-secret');
+  register_setting('tmp-option', 'twitter-access-token');
+  register_setting('tmp-option', 'twitter-access-secret');
+}
+function tmp_user_page()
+{
+  global $current_user;
+  get_currentuserinfo();
+  add_option("ID-".$current_user->ID);
+  if(isset($_POST['twitter']))
+    update_option("ID-".$current_user->ID,$_POST['twitter']);
+  //echo get_option($current_user->user_login);
+  //echo $current_user->ID;
+  echo "<div class=\"wrap\">";
+  echo "<h2> Tweet My Post</h2>";
+  echo "<form method=\"post\" action=\"?page=tmp_user_page\">";
+  settings_fields( 'tmp-option' );
+  //do_settings_fields('tmp-option');
+  echo "<table class=\"form-table\">";
+  echo "<tr valign=\"top\"><th scope=\"row\">Your Twitter Handle</th>";
+  echo "<td>@<input type=\"text\" name=\"twitter\" value=\"".get_option("ID-".$current_user->ID)."\"/></td>";
+  echo "</tr>";
+  echo "</table><p class=\"submit\"><input type=\"submit\" class=\"button-primary\" value=\"Save Changes\" /></p></form></div>";
+}
+function tmp_api_page()
+{
+  echo "<div class=\"wrap\">";
+  echo "<h2> Tweet My Post - Your Twitter API Keys and Access Tokens</h2>";
+  echo "<h3>Create Your App & Get Following Details at <a href=\"https://dev.twitter.com/apps\" target=\"_blank\">https://dev.twitter.com/apps</a></h3>"; 
+  echo "<form method=\"post\" action=\"options.php\">";
+  settings_fields( 'tmp-option' );
+  //do_settings_fields('tmp-option');
+  echo "<table class=\"form-table\">";
+  echo "<tr valign=\"top\"><th scope=\"row\">Twitter Consumer Key:</th>";
+  echo "<td><input type=\"text\" name=\"twitter-consumer-key\" value=\"".get_option("twitter-consumer-key")."\"/></td>";
+  echo "</tr>";
+  echo "<tr valign=\"top\"><th scope=\"row\">Twitter Consumer Secret:</th>";
+  echo "<td><input type=\"text\" name=\"twitter-consumer-secret\" value=\"".get_option("twitter-consumer-secret")."\"/></td>";
+  echo "</tr>";
+  echo "<tr valign=\"top\"><th scope=\"row\">Twitter Access Token:</th>";
+  echo "<td><input type=\"text\" name=\"twitter-access-token\" value=\"".get_option("twitter-access-token")."\"/></td>";
+  echo "</tr>";
+  echo "<tr valign=\"top\"><th scope=\"row\">Twitter Access Token Secret:</th>";
+  echo "<td><input type=\"text\" name=\"twitter-access-secret\" value=\"".get_option("twitter-access-secret")."\"/></td>";
+  echo "</tr>";
+  echo "</table><p class=\"submit\"><input type=\"submit\" class=\"button-primary\" value=\"Save Changes\" /></p></form></div>";
+}
+function add_tmp_page()
+{
+  add_users_page( "Tweet My Post", "Tweet My Post", level_1, "tmp_user_page", "tmp_user_page");
+  add_menu_page( "TMP - Twitter", "TMP - Twitter ", level_8,"tmp_admin_page", 'tmp_api_page');
+}
+register_activation_hook(__FILE__, 'tmp_activate' );
+load_plugin_textdomain('tweet-my-post', false, basename( dirname( __FILE__ ) ) . '/languages' );
+?>
